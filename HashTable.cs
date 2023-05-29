8898f899eee48486f241bb9533e58de6678ea972
Chetan <chetansadvelkar4@gmail.com>
2023-05-29 11:44:23 +0530

    Add: Hash table in C#.

---
+++ HashTable.cs
@@ -0,0 +1,84 @@
+﻿namespace HashTable
+{
+
+    using System;
+    using System.Collections.Generic;
+
+    class HashTable
+    {
+        private LinkedList<KeyValuePair<int, string>>[] table;
+        private int size;
+
+        public HashTable(int initialSize)
+        {
+            size = initialSize;
+            table = new LinkedList<KeyValuePair<int, string>>[size];
+        }
+
+        private int GetHash(int key)
+        {
+            return key % size;
+        }
+
+        public void Insert(int key, string value)
+        {
+            int index = GetHash(key);
+
+            if (table[index] == null)
+            {
+                table[index] = new LinkedList<KeyValuePair<int, string>>();
+            }
+
+            foreach (KeyValuePair<int, string> pair in table[index])
+            {
+                if (pair.Key == key)
+                {
+                    throw new ArgumentException("Key already exists in the hash table.");
+                }
+            }
+
+            table[index].AddLast(new KeyValuePair<int, string>(key, value));
+        }
+
+        public bool Remove(int key)
+        {
+            int index = GetHash(key);
+
+            if (table[index] != null)
+            {
+                LinkedListNode<KeyValuePair<int, string>> currentNode = table[index].First;
+
+                while (currentNode != null)
+                {
+                    if (currentNode.Value.Key == key)
+                    {
+                        table[index].Remove(currentNode);
+                        return true;
+                    }
+
+                    currentNode = currentNode.Next;
+                }
+            }
+
+            return false;
+        }
+
+        public string GetValue(int key)
+        {
+            int index = GetHash(key);
+
+            if (table[index] != null)
+            {
+                foreach (KeyValuePair<int, string> pair in table[index])
+                {
+                    if (pair.Key == key)
+                    {
+                        return pair.Value;
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace HashTable
+{
+
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashTable hashTable = new HashTable(10);
+
+            // Inserting key-value pairs
+            hashTable.Insert(5, "Value 1");
+            hashTable.Insert(15, "Value 2");
+            hashTable.Insert(25, "Value 3");
+
+            // Retrieving values
+            Console.WriteLine("Value for key 5: " + hashTable.GetValue(5));
+            Console.WriteLine("Value for key 15: " + hashTable.GetValue(15));
+            Console.WriteLine("Value for key 25: " + hashTable.GetValue(25));
+
+            // Removing a key-value pair
+            hashTable.Remove(15);
+
+            // Retrieving values after removal
+            Console.WriteLine("Value for key 15 after removal: " + hashTable.GetValue(15));
+        }
+    }
+}
